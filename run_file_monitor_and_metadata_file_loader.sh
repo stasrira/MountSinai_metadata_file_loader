11a13ce6f1713d0e353085c07e721d3229662e2e
Stas Rirak <stasrira@yahoo.com>
2020-08-17 17:39:38 -0400

    Adds command line files to run application in one click. Also adds another command line file to run file monitoring tool before running the metadata loader.

---
+++ run_file_monitor_and_metadata_file_loader.sh
@@ -0,0 +1,7 @@
+# execute file monitoring applicaton by calling its command file
+/ext_data/shared/.apps/monitor_files/monitor_files_v1.0/run_file_monitoring.sh
+# execute metadata file loader
+cd /ext_data/shared/.apps/metadata_file_loader/metadata_file_loader_v2.0
+source .venv/bin/activate
+python metadata_file_loader.py
+deactivate
